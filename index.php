d2ae40215cf64ad5f5ae90cef0ee05f4e80181d5
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-21 15:09:20 +0200

    Header completed

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+    $faqs = [
+
+    ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+        <div class="top-header">
+            <div class="left">
+                <div class="img-wrapper">
+                    <img src="img/Google-Logo.png" alt="Logo Google">
+                </div>
+
+                <h2>Privacy e Termini</h2>
+            </div>
+
+            <div class="right">
+                <button>Accedi</button>
+            </div>
+        </div>
+
+        <div class="bottom-header">
+            <ul>
+                <li>
+                    <a href="">Introduzione</a>  
+                </li>
+
+                <li>
+                    <a href="">Norme sulla privacy</a>
+                </li>
+
+                <li>
+                    <a href="">Termini di servizio</a>
+                </li>
+
+                <li>
+                    <a href="">Tecnologie</a>
+                </li>
+
+                <li id="domande">
+                    <a href="">Domande Frequenti</a>
+                </li>
+            </ul>
+        </div>
+
+        <hr>
+
+    </header>
+
+</body>
+</html>
